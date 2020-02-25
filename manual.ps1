74e42492ad9e83c04f27ddc72629ec4cdf182da1
Atif-git <59650791+Atif-git@users.noreply.github.com>
2020-02-25 17:26:53 +0530

    uploading another PS script for manual entry

---
+++ manual.ps1
@@ -0,0 +1,9 @@
+﻿Connect-AzAccount
+
+$key= @(Get-AzKeyVaultKey -VaultName Atif-keyvault) #| Select Name
+$secret= @(Get-AzKeyVaultSecret -VaultName Atif-keyvault) #| Select Name | Out-File C:\filename.txt
+
+Write-host $key[0].Name = $secret[0].Name
+Write-Host $key[1].Name = $secret[1].Name
+
+Get-ChildItem -Recurse $source |  ?{-not $_.PSIsContainer} | ForEach-Object {Audit-File $_} | Sort-Object fullname | Select FullName,CreationTime,LastWriteTime,Length,Owner | Out-File C:\filename.txt
